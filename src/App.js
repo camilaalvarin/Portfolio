af392766905ce37afe31d0fe2e62b2292121bbbe
Camila Alvarin <camilaalvarin@gmail.com>
2022-05-13 16:08:47 -0300

    little by little 4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Intro from './components/Intro/Intro'
 import About from './components/About/About';
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return ( 
       <div className="App">
+        <Navbar />
         <Intro />
         <About />
         <Projects />
